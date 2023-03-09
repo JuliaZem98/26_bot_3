3e844ae51c4aeaacef91cba6698e8ff8e8b6dedf
JuliaZem98 <123088610+JuliaZem98@users.noreply.github.com>
2023-03-09 15:17:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
-# This is a sample Python script.
-import decouple
+from aiogram.utils import executor
+import logging
+from config import db
+from handler import client, callback, extra, admin, fsm_anketa
 
-TOKEN = ''
+fsm_anketa.reg_hand_anketa(db)
+admin.reg_ban(db)
+client.reg_client(db)
+callback.reg_hand_callback(db)
+extra.reg_hand_extra(db)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-p = ''
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import random
-
-
-class A1:
-    def __init__(self, a):
-        self.a = a
-
-
-a = A1('a')
-print(a)
-import colorama
-print(colorama.Back.GREEN)
-print(colorama.Back.GREEN)
-print(colorama.Back.GREEN)
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(db, skip_updates=True)
